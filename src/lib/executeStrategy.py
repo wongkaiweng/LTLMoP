e461615b26a72d72fdd1ef7b0532b848a5ed9a96
wongkaiweng <wongkaiweng@gmail.com>
2014-04-07 19:22:46 -0400

    separated normal execution from fastslow. Now are two different functions in executeStrategy.py

--- src/lib/executeStrategy.py
+++ src/lib/executeStrategy.py
@@ -12,6 +12,7 @@ class ExecutorStrategyExtensions(object):
         super(ExecutorStrategyExtensions, self).__init__()
 
         self.last_next_states= []
+        self.last_sensor_state = {}
         self.next_state      = None
         self.current_region  = None
         self.next_region     = None
@@ -44,32 +45,11 @@ class ExecutorStrategyExtensions(object):
         """
         Run, run, run the automaton!  (For one evaluation step)
         """
+        # find current region
+        self.current_region = self.strategy.current_state.getPropValue('region')
+
         # Take a snapshot of our current sensor readings
         sensor_state = self.hsub.getSensorValue(self.proj.enabled_sensors)
-
-        # find current region
-        if self.proj.compile_options['fastslow']:
-            # find current region based on region sensors and remove those sensors from sensor_state
-            # finally add the "regionCompleted" sensor with region object
-            sensor_region = dict((k,v) for k, v in  sensor_state.iteritems() if k.endswith('_rc'))
-            for key, value in sensor_region.iteritems():
-                del sensor_state[key]
-            sensor_region_names = [k for k, v in  sensor_region.iteritems() if v]
-
-            # check we are in any region. (can be because of timing or driving issues that the robot is not in any region)
-            if sensor_region_names:
-                sensor_region_name = sensor_region_names[0].replace('_rc','')
-                decomposed_region_names = self.proj.regionMapping[sensor_region_name]
-                self.prev_decomposed_region_names = decomposed_region_names
-            else: 
-                logging.info('not inside any region!')
-                decomposed_region_names  = self.prev_decomposed_region_names
-
-            sensor_state['regionCompleted'] = self.proj.rfi.regions[self.proj.rfi.indexOfRegionWithName(decomposed_region_names[0])]  #should only be one in our case. not taking care of convexify now 
-            self.current_region = sensor_state['regionCompleted']
-            #self.current_region = self.strategy.current_state.getPropValue('regionCompleted')
-        else:
-            self.current_region = self.strategy.current_state.getPropValue('region')
 
         # Let's try to transition
         # TODO: set current state so that we don't need to call from_state
@@ -93,26 +73,16 @@ class ExecutorStrategyExtensions(object):
                 next_states.remove(self.strategy.current_state)
 
             self.next_state = random.choice(next_states)
-            # find next region
-            if self.proj.compile_options['fastslow']:
-                self.next_region = self.strategy.current_state.getPropValue('region')
-            else:
-                self.next_region = self.next_state.getPropValue('region')
+            self.next_region = self.next_state.getPropValue('region')
 
             self.postEvent("INFO", "Currently pursuing goal #{}".format(self.next_state.goal_id))
 
             # See what we, as the system, need to do to get to this new state
-            if self.proj.compile_options['fastslow']:
-                self.transition_contains_motion = self.next_region is not None and (self.next_region != self.strategy.current_state.getPropValue('regionCompleted'))
-                
-                # Run actuators before motion
-                self.updateOutputs(self.next_state)
-                
-            else:
-                self.transition_contains_motion = self.next_region is not None and (self.next_region != self.current_region)
-                if self.transition_contains_motion:
-                    # We're going to a new region
-                    self.postEvent("INFO", "Heading to region %s..." % self.next_region.name)
+            self.transition_contains_motion = self.next_region is not None and (self.next_region != self.current_region)
+
+            if self.transition_contains_motion:
+                # We're going to a new region
+                self.postEvent("INFO", "Heading to region %s..." % self.next_region.name)
 
             self.arrived = False
 
@@ -125,12 +95,89 @@ class ExecutorStrategyExtensions(object):
             # TODO: Check to see whether actually inside next region that we expected
             if self.transition_contains_motion:
                 self.postEvent("INFO", "Crossed border from %s to %s!" % (self.current_region.name, self.next_region.name))
-                if self.proj.compile_options['fastslow']:
-                    self.postEvent("INFO", "Heading to region %s..." % self.next_region.name)
 
             if not self.proj.compile_options['fastslow']:
                 # Run actuators after motion
                 self.updateOutputs(self.next_state)
+
+            self.strategy.current_state = self.next_state
+            self.last_next_states = []  # reset
+
+            self.postEvent("INFO", "Now in state %s (z = %s)" % (self.strategy.current_state.state_id, self.strategy.current_state.goal_id))
+
+    def runStrategyIterationInstanteousAction(self):
+        """
+        Run, run, run the strategy with instantanous action!  (For one evaluation step)
+        """
+        # Take a snapshot of our current sensor readings
+        sensor_state = self.hsub.getSensorValue(self.proj.enabled_sensors)
+
+        # find current region based on region sensors and remove those sensors from sensor_state
+        # finally add the "regionCompleted" sensor with region object
+        sensor_region = dict((k,v) for k, v in  sensor_state.iteritems() if k.endswith('_rc'))
+        for key, value in sensor_region.iteritems():
+            del sensor_state[key]
+        sensor_region_names = [k for k, v in  sensor_region.iteritems() if v]
+
+        # check we are inside any region, if not we will use the previous region detected. (timing issue)
+        if sensor_region_names:
+            sensor_region_name = sensor_region_names[0].replace('_rc','')
+            decomposed_region_names = self.proj.regionMapping[sensor_region_name]
+            self.prev_decomposed_region_names = decomposed_region_names
+        else:
+            logging.info('not inside any region!')
+            decomposed_region_names  = self.prev_decomposed_region_names
+
+        sensor_state['regionCompleted'] = self.proj.rfi.regions[self.proj.rfi.indexOfRegionWithName(decomposed_region_names[0])]  #should only be one in our case. not taking care of convexify now
+        self.current_region = sensor_state['regionCompleted']
+
+        # Let's try to transition
+        # TODO: set current state so that we don't need to call from_state
+        next_states = self.strategy.findTransitionableStates(sensor_state, from_state= self.strategy.current_state)
+
+        # Make sure we have somewhere to go
+        if len(next_states) == 0:
+            # Well darn!
+            logging.error("Could not find a suitable state to transition to!")
+            return
+
+        # See if we're beginning a new transition
+        if next_states != self.last_next_states:
+            # NOTE: The last_next_states comparison is also to make sure we don't
+            # choose a different random next-state each time, in the case of multiple choices
+            self.last_next_states = next_states
+
+            # Only allow self-transitions if that is the only option!
+            if len(next_states) > 1 and self.strategy.current_state in next_states:
+                next_states.remove(self.strategy.current_state)
+
+            self.next_state = random.choice(next_states)
+            # find next region
+            self.next_region = self.strategy.current_state.getPropValue('region')
+            self.postEvent("INFO", "Currently pursuing goal #{}".format(self.next_state.goal_id))
+
+            # See what we, as the system, need to do to get to this new state
+            self.transition_contains_motion = self.next_region is not None and (self.next_state.getPropValue('regionCompleted') != self.strategy.current_state.getPropValue('regionCompleted'))
+
+            # Run actuators before motion
+            self.updateOutputs(self.next_state)
+
+            # check if we have changes in sensor values
+            if self.last_sensor_state != sensor_state and self.strategy.current_state.getPropValue('regionCompleted') == self.next_state.getPropValue('regionCompleted'):
+                self.arrived = True
+            else:
+                self.arrived = False
+            self.last_sensor_state = sensor_state
+
+        if not self.arrived:
+            # Move one step towards the next region (or stay in the same region)
+            self.arrived = self.hsub.gotoRegion(self.current_region, self.next_region)
+
+        # Check for completion of motion
+        if self.arrived and self.next_state != self.strategy.current_state:
+            if self.transition_contains_motion:
+                self.postEvent("INFO", "Crossed border from %s to %s!" % (self.current_region.name, self.next_state.getPropValue('regionCompleted').name))
+                self.postEvent("INFO", "Heading to region %s..." % self.next_region.name)
 
             self.strategy.current_state = self.next_state
             self.last_next_states = []  # reset
