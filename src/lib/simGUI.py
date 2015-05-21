66bce687d3271e6b48c127b90c5d342ac0192310
Catherine Wong <wongkaiweng@gmail.com>
2015-05-21 19:05:45 -0400

    fixed simGUI when no map is supported with white background

--- src/lib/simGUI.py
+++ src/lib/simGUI.py
@@ -273,11 +273,12 @@ class SimGUI_Frame(wx.Frame):
 
     def onResize(self, event=None): # wxGlade: SimGUI_Frame.<event_handler>
         size = self.window_1_pane_1.GetSize()
-        self.mapBitmap = wx.EmptyBitmap(size.x, size.y)
-        self.mapScale = mapRenderer.drawMap(self.mapBitmap, self.proj.rfi, scaleToFit=True, drawLabels=mapRenderer.LABELS_ALL_EXCEPT_OBSTACLES, memory=True)
+        if self.proj.rfi:
+            self.mapBitmap = wx.EmptyBitmap(size.x, size.y)
+            self.mapScale = mapRenderer.drawMap(self.mapBitmap, self.proj.rfi, scaleToFit=True, drawLabels=mapRenderer.LABELS_ALL_EXCEPT_OBSTACLES, memory=True)
 
-        self.Refresh()
-        self.Update()
+            self.Refresh()
+            self.Update()
 
         if event is not None:
             event.Skip()
