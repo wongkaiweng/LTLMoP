92341201fa892120df5b82b0292c9b1582291f7f
Catherine Wong <wongkaiweng@gmail.com>
2015-10-19 22:44:59 -0400

    added new helper function to convert ltl to slugsin

---
+++ src/lib/helperFns/convertLTLtoSLUGS.py
@@ -0,0 +1,34 @@
+import sys,os
+
+# Add the conversion script to our path
+p = os.path.abspath(__file__)
+print os.path.join(os.path.dirname(p), "../")
+sys.path.append(os.path.join(os.path.dirname(p), "../../"))
+
+slugs_converter_path = os.path.join(os.path.join(os.path.dirname(p), "../../"), "etc", "slugs", "tools")
+sys.path.insert(0, slugs_converter_path)
+from translateFromLTLMopLTLFormatToSlugsFormat import performConversion
+
+def prepareSlugsInput(proj_dir):
+    """ Convert from JTLV input format (.smv+.ltl) to Slugs input format (.slugsin)
+        using the script provided by Slugs.
+
+        This is a stop-gap fix; eventually we should just produce the input
+        directly instead of using the conversion script. """
+
+    # Call the conversion script
+    with open(proj_dir + ".slugsin", "w+") as f:
+        # TODO: update performConversion so we don't have to do stdout redirection
+        sys.stdout = f
+        performConversion(proj_dir + ".smv", proj_dir + ".ltl")
+        sys.stdout = sys.__stdout__
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        file_dir = os.path.join(os.path.join(os.path.dirname(p), "../../"), sys.argv[1])
+    else:
+        file_dir = os.path.join(os.path.join(os.path.dirname(p), "../../"), "examples/firefighting/firefighting")
+    prepareSlugsInput(file_dir)
+    print "Converted " + file_dir + '.ltl to ' + file_dir + '.slugsin!\n'
