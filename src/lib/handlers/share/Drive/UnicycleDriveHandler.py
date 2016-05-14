17ad3bd29a185525591088655ab4158b15a679f1
jadecastro <jdc1177@gmail.com>
2016-05-14 09:18:57 -0400

    add missing drive handler for unicycle

---
+++ src/lib/handlers/share/Drive/UnicycleDriveHandler.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+=======================================================
+UnicycleDrive.py - Unicycle Drive Handler
+=======================================================
+
+"""
+
+import lib.handlers.handlerTemplates as handlerTemplates
+
+class UnicycleDriveHandler(handlerTemplates.DriveHandler):
+    def __init__(self, executor, shared_data,multiplier,maxspeed):
+        """
+        Passes velocity requests directly through.
+        Used for ideal holonomic point robots.
+
+        multiplier (float): Scale the velocity from motionControlHandler (default=1.0,min=1.0,max=50.0)
+        maxspeed (float): Max speed allowed (default=999.0)
+        """
+        try:
+            self.loco = executor.hsub.getHandlerInstanceByType(handlerTemplates.LocomotionCommandHandler)
+            self.coordmap = executor.hsub.coordmap_lab2map
+        except NameError:
+            print "(DRIVE) Locomotion Command Handler not found."
+            exit(-1)
+
+        self.mul = multiplier
+        self.max = maxspeed
+
+    def setVelocity(self, vx, vy, w, theta=0):
+        vx = max(min(vx*self.mul,self.max),-self.max)
+        vy = max(min(vy*self.mul,self.max),-self.max)
+        w = max(min(w,2),-2)
+
+        #print "VEL:%f,%f" % tuple(self.coordmap([x, y]))
+        # print 'Drive handler inputs x:' + str(x) + ' y:' + str(y)
+        self.loco.sendCommand([vx,vy,w])
+
